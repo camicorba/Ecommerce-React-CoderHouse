3a6d5238b6cd8bfb0ce60552cf20d2ea69792315
camila corbalan <camincorbalan@gmail.com>
2022-02-18 01:30:31 -0300

    desafio navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,24 @@
+import logoh1 from '../images/logoh1.png';
+import './NavBar.css'
+const NavBar = () => {
+    return (
+        <div className='menu'>
+            <div>
+                <img className='menu-logo' alt="logo" src={ logoh1 } />
+            </div>
+            <div>
+                <ul>
+                    <li className='menu-item'>
+                        <a className='menu-link' href='##'>INICIO</a>
+                        <a className='menu-link' href='##'>PRODUCTOS</a>
+                        <a className='menu-link' href='##'>QUIENES SOMOS</a>
+                        <a className='menu-link' href='##'>CONTACTO</a>
+                    </li>
+                </ul>
+            </div>
+
+        </div>
+    )
+}
+
+export default NavBar
