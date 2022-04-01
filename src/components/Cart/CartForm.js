23f154deeff882794e0649fdd6957e5d7101b65f
camila corbalan <camincorbalan@gmail.com>
2022-04-01 19:55:57 -0300

    guardar orden - Firebase

--- src/components/Cart/CartForm.js
+++ src/components/Cart/CartForm.js
@@ -7,37 +7,36 @@ const CartForm = () => {
     const [dataForm, setdataForm] = useState({
         name:'', phone:'', email:'', adress:'', city:''
     })
-    const [id, setid] = useState('')
+    // const [id, setid] = useState('')
+
     const generarOrden = async(e) =>{
         e.preventDefault();
-        let orden= {}
+        let orden= {};
         orden.buyer = dataForm
         orden.total= totalPrice()
         orden.items= cartList.map(cartItem=>{
             let id= cartItem.id
-            let name = cartItem.name
+            let title = cartItem.title
             let price = cartItem.price
-            return {id, name, price}
+            return {id, title, price}
         })
+        console.log(orden)
 
         const db = getFirestore()
         const queryCollectionSet = collection(db, 'orders')
         addDoc(queryCollectionSet, orden)
-        .than(resp=>setid(resp.id))
-        .catch(err=>console.error(err))
-        .finally(console.log('Orden finalizada'))
+            .catch(err=>console.error(err))
+            .finally(()=>console.log('Orden finalizada'))
     }
-    const handleChange= (e)=>{
-        setdataForm({
-            ...dataForm,
-            [e.target.name]: e.target.value
-        })
+    const handleChange = (e)=>{
+        setdataForm({ ...dataForm, [e.target.name]: e.target.value})
     }
+    console.log(dataForm)
 
     return(
         <div className="cont-form">
             <h2>Completa tus datos:</h2>
-            <form>
+            <form  onSubmit={generarOrden}>
                 <h3>Datos de Contacto</h3>
                 <input type='text' name='name' value={dataForm.name} onChange={handleChange} placeholder='Nombre Completo'></input>
                 <br/>
@@ -46,14 +45,14 @@ const CartForm = () => {
                 <input type='email' name='email' value={dataForm.email} onChange={handleChange} placeholder='Email'></input>
                 <br/>
                 <input type='checkbox' value='check'></input>
-                <a>Deseo recibir ofertas y novedades al mail</a>
+                <p>Deseo recibir ofertas y novedades al mail</p>
                 <br/>
                 <h3>Datos de Envio</h3>
                 <input type='text' name='adress' value={dataForm.adress} onChange={handleChange} placeholder='Direccion'></input>
                 <br/>
                 <input type='text' name='city' value={dataForm.city} onChange={handleChange} placeholder='Ciudad'></input>
                 <br/>
-                <button onClick={generarOrden}>Confirmar datos</button>
+                <button>Confirmar datos</button>
             </form>
             
 
